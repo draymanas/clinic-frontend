2c381b24c054c642a1b452bd9b6c04f08feda762
draymanas <draymanas2016@gmail.com>
2026-05-28 18:50:18 +0300

    اضافة الخدمات

--- src/ServiceDetails.js
+++ src/ServiceDetails.js
@@ -100,7 +100,7 @@ const sendToTelegram = async (e) => {
           onClick={() => window.open('https://www.doctoreg.online/dr/40', '_blank')} 
           style={{ width: '100%', marginTop: '20px', fontSize: '24px', padding: '15px', background: '#1a73e8', color: '#fff', border: 'none', borderRadius: '10px', fontWeight: 'bold', cursor: 'pointer' }}
         >
-          حجز موعدالان فرع أكتوبر
+          حجز موعد الان فرع أكتوبر
         </button>
 
         <button 
