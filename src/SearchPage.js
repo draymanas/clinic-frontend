96b126d0f5ffe440397693a514267bd918033e64
draymanas <draymanas2016@gmail.com>
2026-09-14 19:02:25 +0300

    admin updates

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -637,7 +637,7 @@ return (
                             title="اضغط لعرض الملف الشخصي ومواعيد الطبيب"
                            style={{
                                 position: 'relative',
-                                backgroundColor: '#39aef1', // أزرق سماوي فاتح جداً ونظيف
+                                backgroundColor: '#8ed3fc', // أزرق سماوي فاتح جداً ونظيف
                                 padding: '25px',
                                 borderRadius: '24px',
                                 width: '310px',
